2c49bedd75516df090b0ee6b2b175d062102d467
gerardosdl <gerardo.sdl.os@gmail.com>
2025-05-19 23:34:24 -0400

    Clean up some comments, refactor edit and add item functionality, Delete price not working

--- controllers/prices.js
+++ controllers/prices.js
@@ -8,20 +8,7 @@ const ensureLoggedIn = require('../middleware/ensure-logged-in');
 // This is how we can more easily protect ALL routes for this router
 router.use(ensureLoggedIn);
 
-// ALL paths start with '/'
-
-// index action
-// GET /items
-// router.get('/', ensureLoggedIn, async (req, res) => {
-//   const items = await Item.find({}).sort('-createdAt');
-//   res.render('items/index.ejs', { items });
-// });
-
-// GET /items/:id/prices/new
-// Example of a protected route
-// router.get('items/:id/prices/new', (req, res) => {
-//   res.render('items/new.ejs');
-// });
+// ALL paths start with '/
 
 router.post('/items/:id/prices', async (req, res) => {
   try {
@@ -38,55 +25,19 @@ router.post('/items/:id/prices', async (req, res) => {
   }
 });
 
-// router.get('/:id', ensureLoggedIn, async (req, res) => {
-//   const item = await Item.findById(req.params.id).populate('user');
-//   res.render('items/show.ejs', { item })
-// });
-
-// router.delete('/:id', ensureLoggedIn, async (req, res) => {
-//   try {
-//     const item = await Item.findById(req.params.id);
-//     if (item.user.equals(req.user._id)) {
-//       await item.deleteOne();
-//       res.redirect('/items');
-//     } else {
-//       res.send("You are not permitted to delete this item");
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     res.redirect('/');
-//   } 
-// });
-
-// router.get('/:id/edit', ensureLoggedIn, async (req, res) => {
-//   try {
-//     const currentItem = await Item.findById(req.params.id);
-//     res.render('items/edit.ejs', {
-//       item: currentItem,
-//     }); 
-//   } catch (err) {
-//     console.log(err);
-//     res.redirect('/');
-//   }
-// });
-
-// router.get('/:id', ensureLoggedIn, async (req, res) => {
-
-// });
-
-// router.put('/:id', ensureLoggedIn, async (req, res) => {
-//   try {
-//     const currentItem = await Item.findById(req.params.id);
-//     if (currentItem.user.equals(req.user._id)) {
-//       await currentItem.updateOne(req.body);
-//       res.redirect('/items');
-//     } else {
-//       res.send("Denied");
-//       }
-//     } catch (err) {
-//       console.log(err);
-//       res.redirect('/');
-//   }
-// });
+router.delete('/prices/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (price.user.equals(req.user._id)) {
+      await price.deleteOne();
+      res.redirect(`/items/${req.params.id}`);
+    } else {
+      res.send("You are not permitted to delete this price");
+    }
+  } catch (err) {
+    console.log(err);
+    res.redirect(`/items/${req.params.id}`);
+  } 
+});
 
 module.exports = router;
